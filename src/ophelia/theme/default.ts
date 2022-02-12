ba585f20fdda7af97c76b1db0eacb0a5d9edd7fb
Renzo Sevilha <sevilharenzo@gmail.com>
2022-02-11 21:45:12 -0300

    feat(theme): add theme styles

---
+++ src/ophelia/theme/default.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    font: { main: '#FEF3E8' },
+    background: {},
+  },
+  spaces: {
+    sides: 24,
+  },
+  dimensions: {
+    font: {
+      sm: 16,
+    },
+  },
+};
+
+export default theme;
--- src/ophelia/theme/light.ts
+++ src/ophelia/theme/light.ts
@@ -1,7 +1,17 @@
-import { theme, Theme } from './';
+import defaultTheme from './default';
+import { Theme } from './';
 
 const light: Theme = {
-  ...theme,
+  ...defaultTheme,
+  colors: {
+    ...defaultTheme.colors,
+    background: {
+      ...defaultTheme.colors.background,
+      main: '#feeacc',
+      secundary: '#190117',
+      terciary: '#490912',
+    },
+  },
 };
 
 export default light;
